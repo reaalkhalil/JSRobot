15e4339c00bbfa4544e1a4bd635eaf3bf48965b5
Reaal Khalil <reaal@reaal.me>
2016-11-15 13:13:41 +0000

    added effects layer

---
+++ scripts/Effects.js
@@ -0,0 +1,34 @@
+var mozart = require('mozart');
+
+var Effects = mozart(function(prototype, _, _protected, __, __private) {
+	prototype.init = function() {
+		__(this).effects = {};
+		__(this).mass = undefined;
+		__(this).k = {x: 0, y: 0};
+	};
+	prototype.getK = function(){ return __(this).k; };
+	prototype.getType = function(){ return "effects"; };
+	prototype.getBox = function(){ return 0; };
+	prototype.toBeDestroyed = function(){ return false; };
+	prototype.isFixed = function(){ return true; };
+
+	prototype.getMass = function(){ return __(this).mass; };
+	prototype.play = function(name,options){
+		if(options.x && options.y){
+			__(this).effects[name].setPos(options.x, options.y);
+		}
+		__(this).effects[name].play();
+	};
+	prototype.render = function(){
+		for(var i in __(this).effects){
+			__(this).effects[i].redraw();
+		}
+	};
+	prototype.addEffect = function(name, sprite){
+		__(this).effects[name] = sprite;
+		__(this).effects[name].setParent(this);
+	};
+});
+
+
+var effects = new Effects();
