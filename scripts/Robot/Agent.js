27cf7fb4d70adb20795476492d2cd9d5e28858b3
Reaal Khalil <reaal@reaal.me>
2016-11-17 13:35:09 +0000

    moved robot related stuff to robot dir

---
+++ scripts/Robot/Agent.js
@@ -0,0 +1,42 @@
+var mozart = require('mozart');
+
+var agent = new Behavior(function(bodyPriv, bodyPubl){
+	if(!bodyPubl.isAgent()){return;}
+	if(bodyPriv.properties.nextMove){
+		var options = bodyPriv.properties.nextMove.split(':');
+			console.log(options[0]);
+		if(options[0] == "jump"){
+			bodyPriv.k.ay = -15;
+			bodyPriv.properties.energy -= 1;
+		}else if(options[0] == "move"){
+			var amount = Number(options[1]);
+			if(Math.abs(amount) > 20){amount = 20 * Math.sign(amount);}
+			bodyPriv.k.ax = amount;
+			bodyPriv.properties.energy -= Math.abs(amount)/10;
+		}else if(options[0] == "gun"){
+			bodyPriv.getSprite("gun").show();
+			bodyPriv.getSprite("robot").hide();
+			bodyPriv.k.ax -= 0.1;
+			var bullet = new Body({x: bodyPriv.k.x+35, y:bodyPriv.k.y, vx: 10, type: "bullet", mass: -1, t: engine.getTime(), lifetime: 100});
+			bullet.addSprite(new Sprite({
+				'context': context,
+				x: 0,
+				y: 0,
+				width: 14,
+				height: 10,
+				destwidth: 14,
+				destheight: 10,
+				image: images.bullet
+			}));
+
+			engine.add(bullet);
+			//bodyPriv.engine.priv.world.push(bullet);
+		}
+	}
+	console.log(bodyPriv.properties.energy, bodyPriv.properties.coins);
+	bodyPriv.properties.nextMove = null;
+	propertiesDiv.innerHTML = "Energy: " + bodyPriv.properties.energy +
+							"<br>Coins: " + bodyPriv.properties.coins +
+							"<br>Health: " + bodyPriv.properties.health;
+
+});
---
+++ scripts/Robot/Robot.js
@@ -0,0 +1,102 @@
+var Robot = Body.subclass(function(prototype, _, _protected, __, __private) {
+	prototype.init = function(options) {
+		prototype.super.init.call(this, options);
+	};
+
+	prototype.getOpponentProperties = function(){
+		return __(this).opponent.getK();
+	};
+
+	// nope:
+	prototype.setOpponent = function(op){
+		__(this).opponent = op;
+	};
+	//prototype.getEnergy = function(){ return __(this).energy; };
+	// ^ will need to call super properties energy
+});
+
+
+var RobotOne = Robot.subclass(function(prototype, _, _protected, __, __private) {
+	prototype.init = function(options) {
+		prototype.super.init.call(this, options);
+	};
+	
+	prototype.gun = function(){
+		_protected.super.super.setNextMove.call(this,"gun");
+	};
+	prototype.jump = function(){
+		if(this.onGround()&&Math.abs(this.getK().vy)<1){
+			_protected.super.super.setNextMove.call(this,"jump");
+		}
+	};
+	prototype.move = function(dx){
+		if(dx != Number(dx)){return;}
+		_protected.super.super.setNextMove.call(this,"move:"+dx);
+	};
+
+	prototype.command = function(string){
+		var stringFn = new Function("var robot = this;"+string);
+		stringFn.call(this);
+	};
+
+	prototype.step = function(robot){};
+
+});
+		
+
+
+
+
+/*
+var Robot = Body.subclass(function(prototype, _, _protected, __, __private) {
+	prototype.init = function(x, y, img) {
+		__(this).x = x;
+		__(this).y = y;
+		__(this).image = img;
+		__(this).health = 100;
+		__(this).energy = 100;
+		__(this).coins = 0;
+		elements.push(this);
+		gravitatingObjects.push(this);
+	};
+	prototype.getX = function(){
+		return __(this).x;
+	};
+
+	prototype.getOpponentProperties = function(){
+		return {x: __(this).opponent.getX()};
+	};
+
+	prototype.setOpponent = function(op){
+		__(this).opponent = op;
+	};
+
+	prototype.redraw = function(){
+		context.drawImage(images[__(this).image], __(this).x, __(this).y, 30, 41);
+	};
+
+	_protected.move = function(dx,dy){
+		__(this).x += dx;
+		__(this).y += dy;
+	};
+});
+
+
+var RobotOne = Robot.subclass(function(prototype, _, _protected, __, __private) {
+	prototype.init = function(x, y, img) {
+		prototype.super.init.call(this, x, y, img);
+	};
+
+	prototype.step = function(){
+		var _x = prototype.super.getX.call(this);
+		var _op = prototype.super.getOpponentProperties.call(this);
+		var towardopponent = (_op.x - _x) / Math.abs(_op.x - _x);
+
+		if(Math.abs(_op.x - _x) > 200){
+			_protected.super.move.call(this,towardopponent*5,0);
+		}else{
+		}
+	}
+});
+
+*/
