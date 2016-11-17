86238846ca166cf58d09bd6543dd9d34b0eda1dc
Reaal Khalil <reaal@reaal.me>
2016-11-17 13:35:30 +0000

    moved robot related stuff to robot dir

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,11 @@ define(function (require) {
 	var Collision = require('Physics/Collision');
 	var Engine = require('Physics/Engine');
 	var Body = require('Physics/Body');
-	var Robot = require('Robot');
+	var Robot = require('Robot/Robot');
 	var Sprite = require('Sprite');
 	var Effects = require('Effects');
 	var script = require('script');
+	var Agent = require('Robot/Agent');
 });
 
 // require isn't set up properly
