779d4d10e4be0078eefd2a5d3cf47cd4ecda3c43
Reaal Khalil <reaal@reaal.me>
2016-11-20 13:48:21 +0000

    added flag, bullet, sparkstrip, sparks effect

--- scripts/Builder.js
+++ scripts/Builder.js
@@ -2,30 +2,36 @@ define(['mozart', 'Body', 'Sprite', 'Robot'], function (mozart, Body, Sprite, ro
 RobotOne = robot.RobotOne;
 var Builder = mozart(function(prototype, _, _protected, __, __private) {
 	prototype.init = function() {
-		__(this).imageFiles =	["robot1", "robot1gun", "robot2", "coin", "wall1", "wall2", "wall3",
+		__(this).imageFiles =	["robot1", "robot1gun", "robot1dead", "robot1win", "robot2", "coin", "wall1", "wall2", "wall3",
 			   					"wall4", "wall5", "wall6", "wall7", "wall8", "wall9", "wall10", "battery",
-			   					"coinpop", "batterypop", "bullet", "bulletpop"];
+			   					"coinpop", "batterypop", "bullet", "sparkstrip", "spark", "bulletpop", "flag"];
 		__(this).images = {};
 		__(this).engine = null;
 
 		__(this).sprites = {
 		player: [
 		{name: "gun", x: 5, y: 0, width: 105, height: 108, destwidth: 40, destheight: 41, image: "robot1gun", visible: false},
+		{name: "win", x: 0, y: 0, width: 103, height: 108, destwidth: 39, destheight: 41, image: "robot1win", visible: false},
+		{name: "dead", x: 0, y: 0, width: 101, height: 79, destwidth: 38, destheight: 30, image: "robot1dead", visible: false},
 		{name: "robot", x: 0, y: 0, width: 79, height: 108, destwidth: 30, destheight: 41, image: "robot1"}],
 		coin: [{name: "coin", x: 0, y: 0, width: 120, height: 20, destwidth: 20,
 			   	destheight: 20, image: "coin", numberOfFrames: 6, loop: true, ticksPerFrame: 4 }],
 		battery: [{x: 0, y: 0, width: 96, height: 24, destwidth: 16, destheight: 24, image: "battery",
 			   	numberOfFrames: 6, loop: true, ticksPerFrame: 4}],
 		wall: [{x: 0, y: 0, width: 50, height: 50, destwidth: 40, destheight: 40, image: "wall1" }],
-		bullet: [{x: 0, y: 0, width: 14, height: 10, destwidth: 14, destheight: 10, image: "bullet" }]
+		sparkstrip: [{x: 0, y: 0, width: 84, height: 5, destwidth: 70, destheight: 12.5, image: "sparkstrip", numberOfFrames: 3, loop: true, ticksPerFrame: 2 }],
+		bullet: [{x: 0, y: 0, width: 14, height: 10, destwidth: 14, destheight: 10, image: "bullet" }],
+		flag: [{x: 0, y: 0, width: 50, height: 11, destwidth: 30, destheight: 33, image: "flag", numberOfFrames: 5, loop: true, ticksPerFrame: 4 }]
 		};
 
 		__(this).elements = {
-		player: {x:0,y:0, agent:true, type: "robotc", properties: {energy: 100, coins: 0, health: 100}},
+		player: {x:0,y:0, agent:true, type: "player", properties: {energy: 100, coins: 0, health: 100}},
 		coin: {x:0,y:0, type: "coin"},
 		battery: {x: 0, y: 0, type: "battery"},
 		wall: {x: 0, y: 0, fixed: true, type: "wall", mass: -1},
-		bullet: {x: 0, y: 0, vx: 10, type: "bullet", mass: -1, lifetime: 100}
+		sparkstrip: {x: 0, y: 0, fixed: true, type: "sparkstrip", mass: -1},
+		bullet: {x: 0, y: 0, vx: 10, type: "bullet", mass: -1, lifetime: 100},
+		flag: {x: 0, y: 0, fixed: true, type: "flag", mass: -1}
 		};
 
 	};
@@ -106,6 +112,13 @@ var Builder = mozart(function(prototype, _, _protected, __, __private) {
 			var battery = new Body(__(this).prepareObject("battery", data.batteries[b]));
 			__(this).engine.priv.add(battery);
 		}
+		for(var s in data.sparkstrips){
+			var sparkstrip = new Body(__(this).prepareObject("sparkstrip", data.sparkstrips[s]));
+			__(this).engine.priv.add(sparkstrip);
+		}
+		var flag = new Body(__(this).prepareObject("flag", data.flag));
+		__(this).engine.priv.add(flag);
+
 		var player = new RobotOne(__(this).prepareObject("player", data.player));
 		__(this).engine.priv.add(player);
 
@@ -155,6 +168,23 @@ effects.addEffect("batterypop",new Sprite({
 	visible: false,
 	ticksPerFrame: 2
 }));
+
+effects.addEffect("spark",new Sprite({
+	'context': context,
+	name: "spark",
+	x: 0,
+	y: 0,
+	width: 48,
+	height: 11,
+	destwidth: 36,
+	destheight: 33,
+	image: __(this).images.spark,
+	numberOfFrames: 4,
+	visible: false,
+	ticksPerFrame: 2
+}));
+
+
 
 	};
 
