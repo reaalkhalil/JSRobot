77c4abf9b25bb46f4736a5ad3a58dbc8e64f0f33
Reaal Khalil <reaal@reaal.me>
2017-10-12 13:53:37 +0100

    misc

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 
 var canvas = document.getElementById("canvasID");
+var codearea = document.getElementById("codearea");
 var context = canvas.getContext("2d");
 var fps = 30;
 var Game = {};
@@ -8,25 +9,29 @@ requirejs.config({
     baseUrl: 'scripts',
 });
 
-requirejs(['mozart',
-	   	'../data/level',
-	   	'Behavior',
-	   	'Collision',
-	   	'Builder',
-	   	'Engine',
-	   	'Body',
-	   	'Robot',
-	   	'Sprite',
-	   	'Effects',
-	   	'Agent'],
-function   (mozart, levelData, Behavior, Collision, builder, Engine, Body, robot, Sprite, Effects, Agent) {
+function startLevel(level){
+  requirejs(['mozart',
+  	   	'../data/levels',
+  	   	'Behavior',
+  	   	'Collision',
+  	   	'Builder',
+  	   	'Engine',
+  	   	'Body',
+  	   	'Robot',
+  	   	'Sprite',
+  	   	'Effects',
+  	   	'Agent'],
+  function   (mozart, levelData, Behavior, Collision, builder, Engine, Body, robot, Sprite, Effects, Agent) {
 
-	//agent = Agent;
-	//Robot = robot.Robot;
-	//RobotOne = robot.RobotOne;
-	collide = Collision; // use as a static method
-	effects = new Effects();
-	engine = new Engine();
-	//engine.add(effects);
+  	//agent = Agent;
+  	//Robot = robot.Robot;
+  	//RobotOne = robot.RobotOne;
+    levels = (new levelData()).levels;
+    currentLevel = levels[level - 1];
+  	collide = Collision; // use as a static method
+  	effects = new Effects();
+  	engine = new Engine();
+  	//engine.add(effects);
 
-});
+  });
+}
