53de74feae9b235eae0cf5c016daec6e244825e3
Reaal Khalil <reaal@reaal.me>
2017-12-03 14:58:59 +0000

    remove keyboard control behavior

--- scripts/Behavior.js
+++ scripts/Behavior.js
@@ -17,26 +17,5 @@ var gravitate = new Behavior(function(bodyPriv, bodyPubl){
 	}
 });
 
-
-
-var arrowkeys = [false,false,false,false];
-document.onkeydown = function myFunction() {
-	if(event.keyCode<=40 && event.keyCode>=37){
-		arrowkeys[event.keyCode-37] = true;
-	}
-};
-document.onkeyup = function myFunction() {
-	if(event.keyCode<=40 && event.keyCode>=37){
-		arrowkeys[event.keyCode-37] = false;
-	}
-};
-var keyboardcontrol = new Behavior(function(bodyPriv, bodyPubl){
-	if(bodyPriv.fixed || bodyPriv.type != "player"){return;}
-	if(arrowkeys[0]){ bodyPriv.k.ax += -1; }
-	if(arrowkeys[1]){ bodyPriv.k.ay += -2; }
-	if(arrowkeys[2]){ bodyPriv.k.ax += 1; }
-	if(arrowkeys[3]){ bodyPriv.k.ay += 1; }
-});
-
-return {B: Behavior, g: gravitate, k: keyboardcontrol};
+return {B: Behavior, g: gravitate};
 });
